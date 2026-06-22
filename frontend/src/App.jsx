4a2b449ae8990271fd5222949174653b99a54722
ayaan <ayaanshaikh09092@gmail.com>
2026-06-22 21:21:28 +0530

    Add Dockerfile and .dockerignore; implement initial app structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,12 @@ function App() {
     <>
       <header>
         <Show when="signed-out">
-          
           <SignInButton mode='modal' >
-            <button className='px-4 py-2 bg-black text-amber-50 rounded-2xl border-2 border-amber-50 m-3'>SignIn</button>
+            <button className='px-4 py-2 text-sm bg-black text-amber-50 rounded-2xl border-2 border-amber-50 m-3'>SignIn</button>
           </SignInButton>
        
           <SignUpButton mode='modal'>
-            <button className='px-4 py-2 bg-black text-amber-50 rounded-2xl border-2 border-amber-50 m-3'>SignUp</button>
+            <button className='px-4 py-2 text-sm bg-black text-amber-50 rounded-2xl border-2 border-amber-50 m-3'>SignUp</button>
           </SignUpButton>
         </Show>
         <Show when="signed-in">
