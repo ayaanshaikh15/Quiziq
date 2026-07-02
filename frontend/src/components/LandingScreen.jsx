4f066df80bb52d75bd11d1dd191b643877113a65
ayaan <ayaanshaikh09092@gmail.com>
2026-07-02 20:17:09 +0530

    fix: remove unused Sidebar import in LandingScreen component

--- frontend/src/components/LandingScreen.jsx
+++ frontend/src/components/LandingScreen.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useState } from "react";
 import { ThemeContext } from "../ThemeContext";
 import { Link } from "react-router-dom";
-import Sidebar from "./sidebar";
 function LandingScreen() {
  const { theme } = useContext(ThemeContext);
  
