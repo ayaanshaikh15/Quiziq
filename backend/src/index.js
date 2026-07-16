c5ebfb0c92cd5b5ddd6309a2ecdb173c12ea60f0
ayaan <ayaanshaikh09092@gmail.com>
2026-07-16 22:16:16 +0530

    feat: remove Dockerfile and cron job for simplified backend structure

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,7 @@ import cors from 'cors'
 import Dbconnect from './lib/Db.js';
 import authRouter from './routes/authRoutes.js'
 import userRouter from './routes/userRoutes.js'
-import job from './lib/cron.js';
-import path from 'path';
-import fs from 'fs'
+
 dns.setServers([
   "8.8.8.8", // Google DNS
   "8.8.4.4"
@@ -23,19 +21,11 @@ app.use(
 );
 app.use("/api/auth",authRouter);
 app.use("/api/user",userRouter)
-const publicDir = path.join(process.cwd(),'public')
 
-if(fs.existsSync(publicDir)){
-  app.use(express.static(publicDir));
-  app.get('/{*any}',(req,res,next)=>{
-    res.sendFile(path.join(publicDir,'index.html'),(err)=>next(err))
-  })
-}
 const port = process.env.PORT;
 app.listen(port,()=>{
     Dbconnect();
     console.log(`server is running on ${port}`)
-    if(process.env.NODE_ENV === "production") 
-    job.start()
+   
   
 })
